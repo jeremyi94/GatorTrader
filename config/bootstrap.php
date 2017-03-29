6c2562b55245e7f743a93956fb88b0e2f0b22df4
--quiet <sp17g08@ip-172-31-19-160.us-west-1.compute.internal>
2017-03-29 06:11:14 +0000

    adding bootstrap files

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -220,3 +220,5 @@ Type::build('timestamp')
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
+
+Plugin::load('Ui', ['bootstrap' => false, 'routes' => true]);
