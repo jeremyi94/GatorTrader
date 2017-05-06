671bff2b8bccacdcbd97f68a80668b8350089894
rebecca <rebecca@192.168.1.3>
2017-05-05 21:53:23 -0700

    Categories no longer hard-coded, links fixed from within items controller

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -10,7 +10,34 @@ use App\Controller\AppController;
  * @property \App\Model\Table\UsersTable $Users
  */
 class UsersController extends AppController
-{
+{ public function display(...$path)
+    {
+        $count = count($path);
+        if (!$count) {
+            return $this->redirect('/');
+        }
+        if (in_array('..', $path, true) || in_array('.', $path, true)) {
+            throw new ForbiddenException();
+        }
+        $page = $subpage = null;
+
+        if (!empty($path[0])) {
+            $page = $path[0];
+        }
+        if (!empty($path[1])) {
+            $subpage = $path[1];
+        }
+        $this->set(compact('page', 'subpage'));
+
+        try {
+            $this->render(implode('/', $path),'new');
+        } catch (MissingTemplateException $e) {
+            if (Configure::read('debug')) {
+                throw $e;
+            }
+            throw new NotFoundException();
+        }
+    }
 
     /**
      * Index method
