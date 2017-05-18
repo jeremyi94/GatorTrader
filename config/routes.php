eeb021e06d1f46a4f26baa27aded1ce08ad5a1a3
rebecca <rebecca@192.168.1.3>
2017-05-17 22:23:35 -0700

    working on sell...fixed nav

--- config/routes.php
+++ config/routes.php
@@ -54,7 +54,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'about']);
+    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
